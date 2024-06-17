2a661fd8cecc3b4dde91c9e95fe42942e7572a8a
hahahagame <126378519+hahahagame@users.noreply.github.com>
2024-06-18 02:05:30 +0800

    Add files via upload

---
+++ ITP4915M/Dashboard_planner.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ITP4915M
+{
+    partial class Dashboard_planner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.manageDangerleveBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.deptlbl = new System.Windows.Forms.Label();
+            this.namelbl = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.checkStocklevelBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // manageDangerleveBtn
+            // 
+            this.manageDangerleveBtn.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageDangerleveBtn.Location = new System.Drawing.Point(0, 182);
+            this.manageDangerleveBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.manageDangerleveBtn.Name = "manageDangerleveBtn";
+            this.manageDangerleveBtn.Size = new System.Drawing.Size(200, 50);
+            this.manageDangerleveBtn.TabIndex = 2;
+            this.manageDangerleveBtn.Text = "Manage Danger Level";
+            this.manageDangerleveBtn.UseVisualStyleBackColor = true;
+            this.manageDangerleveBtn.Click += new System.EventHandler(this.inventoryControlBtn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(0, 298);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Data Maintenance";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(0, 356);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "System Control";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.deptlbl);
+            this.panel1.Controls.Add(this.namelbl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1153, 91);
+            this.panel1.TabIndex = 6;
+            // 
+            // deptlbl
+            // 
+            this.deptlbl.AutoSize = true;
+            this.deptlbl.Font = new System.Drawing.Font("Calibri", 10F);
+            this.deptlbl.Location = new System.Drawing.Point(98, 33);
+            this.deptlbl.Name = "deptlbl";
+            this.deptlbl.Size = new System.Drawing.Size(42, 17);
+            this.deptlbl.TabIndex = 1;
+            this.deptlbl.Text = "label7";
+            // 
+            // namelbl
+            // 
+            this.namelbl.AutoSize = true;
+            this.namelbl.Font = new System.Drawing.Font("Calibri", 10F);
+            this.namelbl.Location = new System.Drawing.Point(98, 9);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(42, 17);
+            this.namelbl.TabIndex = 0;
+            this.namelbl.Text = "label5";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.checkStocklevelBtn);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.manageDangerleveBtn);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 91);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(210, 851);
+            this.panel3.TabIndex = 7;
+            // 
+            // checkStocklevelBtn
+            // 
+            this.checkStocklevelBtn.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkStocklevelBtn.Location = new System.Drawing.Point(0, 240);
+            this.checkStocklevelBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkStocklevelBtn.Name = "checkStocklevelBtn";
+            this.checkStocklevelBtn.Size = new System.Drawing.Size(200, 50);
+            this.checkStocklevelBtn.TabIndex = 3;
+            this.checkStocklevelBtn.Text = "Check Stock Level";
+            this.checkStocklevelBtn.UseVisualStyleBackColor = true;
+            this.checkStocklevelBtn.Click += new System.EventHandler(this.checkStocklevelBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ITP4915M.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Dashboard_planner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 942);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Dashboard_planner";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button manageDangerleveBtn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button checkStocklevelBtn;
+        private System.Windows.Forms.Label deptlbl;
+        private System.Windows.Forms.Label namelbl;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ITP4915M/Dashboard_planner.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+using Mysqlx.Crud;
+using Org.BouncyCastle.Asn1.X509;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace ITP4915M
+{
+
+    public partial class Dashboard_planner : Form
+    {
+        string connectionString = DatabaseConfig.ConnectionString;
+
+        string searchDealerQuery = "SELECT * FROM dealer";
+        public static Dashboard_planner instance;
+        private readonly Staff _staffMember;
+
+
+        //private SearchDearler frm2;
+        public Dashboard_planner(Staff staff)
+        {
+            InitializeComponent();
+            ResizePanel();
+     
+            instance = this;
+            this._staffMember = staff;
+            namelbl.Text = "Name : " + staff.Name;
+            deptlbl.Text = "Department : " + staff.Department;
+
+
+
+        }
+
+        private void ResizePanel()
+        {
+            // Get the current screen resolution
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            // Determine the panel size based on the screen resolution
+            int panelWidth, panelHeight;
+            if (screenWidth >= 3840 && screenHeight >= 2160) // 4K resolution
+            {
+                panelWidth = (int)(panel1.Width * 1.0);
+                panelHeight = (int)(panel1.Height * 1.0);
+            }
+            else if (screenWidth >= 2560 && screenHeight >= 1440) // 2K resolution
+            {
+                panelWidth = (int)(panel1.Width * 0.6);
+                panelHeight = (int)(panel1.Height * 0.6);
+            }
+            else // 1080p or lower resolution
+            {
+                panelWidth = (int)(panel1.Width * 0.4);
+                panelHeight = (int)(panel1.Height * 0.4);
+            }
+
+            // Resize the panel
+            panel1.Size = new Size(panelWidth, panelHeight);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+  
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CreOrdBtn_Click(object sender, EventArgs e)
+        {
+  
+
+
+
+
+        }
+
+        private void SercOrdBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void VieOrdBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchDearlerBtn_Click(object sender, EventArgs e)
+        {
+            Form frm2 = new SearchDearler();
+            frm2.ShowDialog();
+
+            //try
+            //{
+            //    frm2 = new SearchDearler(this);
+            //    frm2.MdiParent = this;
+            //    frm2.Show();
+            //    frm2.Visible = true;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Error displaying SearchDearler form: {ex.Message}");
+            //    // Log the exception or perform other error handling
+            //}
+
+        }
+
+
+        private void searchItemBtn_Click(object sender, EventArgs e)
+        {
+            // MySqlConnection conn = new MySqlConnection(connectionString);
+            Form frm = new Searchitem();
+            frm.ShowDialog();
+
+
+
+        }
+
+        private void issueOrderBtn_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void clearItemBtn_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void clearorderBtn_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void Orderlist_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+
+
+
+        private void delOrdBtn_Click(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void updOrdBtn_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form frm = new DataMain_Form();
+            frm.ShowDialog();
+        }
+
+        private void inventoryControlBtn_Click(object sender, EventArgs e)
+        {
+            Form frm = new UpdateDangerLevel();
+            frm.ShowDialog();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inventoryControlPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkStocklevelBtn_Click(object sender, EventArgs e)
+        {
+            Form frm = new CheckStock();
+            frm.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form frm = new CreateUser();
+            frm.ShowDialog();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
